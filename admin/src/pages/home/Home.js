464bdb13856ea9e226394c3bc1c23898e9395796
lvyin1122 <khiwhyzqd@outlook.com>
2022-08-08 20:14:36 +0800

    Update sidebar

--- admin/src/pages/home/Home.js
+++ admin/src/pages/home/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Sidebar from "../../components/sidebar/Sidebar"
+import styled from 'styled-components'
+import { HomeWrapper } from "./style"
+
+const RightWrapper = styled.div`
+    flex: 6;
+`
 
 const Home = () => {
   return (
-    <>
+    <HomeWrapper>
         <Sidebar/>
-    </>
+        <RightWrapper/>
+    </HomeWrapper>
   )
 }
 
